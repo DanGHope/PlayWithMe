f4a155e1840e0c7f09f08b3c76119f7e7907c6d1
unknown <100453916@UOSL15-BXSYS32.oncampus.local>
2015-11-23 16:18:14 -0500

    map works

--- js/main.js
+++ js/main.js
@@ -1,62 +1,44 @@
 var map;
-var userPos;
 
 $(document).ready(function() {
-    $("#goButton").click(function() {
-        loadMap();
-        $("#goButton").hide();
-    });
+    loadMap();
 });
 
 function loadMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: {
-            lat: 0,
-            lng: 0
-        },
-        zoom: 12
-    });
 
-    var infoWindow = new google.maps.InfoWindow({
-        map: map
-    });
+    L.mapbox.accessToken = 'pk.eyJ1IjoiZGFuZ2hvcGUiLCJhIjoiY2loY2M0b3drMDFqbnVlbWF3aGJvYTJ0ZyJ9.NrOUOv9u0AkxEhptyBVexw';
 
-    // Try HTML5 geolocation.
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function(position) {
-            userPos = {
-                lat: position.coords.latitude,
-                lng: position.coords.longitude
-            };
+    var map = L.mapbox.map('map', 'mapbox.streets');
+    var myLayer = L.mapbox.featureLayer().addTo(map);
 
-            infoWindow.setPosition(userPos);
-            infoWindow.setContent('Your Location');
-            map.setCenter(userPos);
-        }, function() {
-            handleLocationError(true, infoWindow, map.getCenter());
-        });
+    var geolocate = $("#geolocate")[0];
+
+    if (!navigator.geolocation) {
+        geolocate.innerHTML = 'Geolocation is not available';
     } else {
-        // Browser doesn't support Geolocation
-        handleLocationError(false, infoWindow, map.getCenter());
+        map.locate();
     }
 
-    var cityCircle = new google.maps.Circle({
-        strokeColor: '#FF0000',
-        strokeOpacity: 0.8,
-        strokeWeight: 2,
-        fillColor: '#FF0000',
-        fillOpacity: 0.35,
-        map: map,
-        center: userPos,
-        radius: 10000.1
+    map.on('locationfound', function(e) {
+        map.fitBounds(e.bounds);
+
+        myLayer.setGeoJSON({
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [e.latlng.lng, e.latlng.lat]
+            },
+            properties: {
+                'title': 'Here I am!',
+                'marker-color': '#ff8888',
+                'marker-symbol': 'star'
+            }
+        });
+
+        geolocate.parentNode.removeChild(geolocate);
     });
 
-    $("#map").show();
-}
-
-function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-    infoWindow.setPosition(pos);
-    infoWindow.setContent(browserHasGeolocation ?
-        'Error: The Geolocation service failed.' :
-        'Error: Your browser doesn\'t support geolocation.');
+    map.on('locationerror', function() {
+        geolocate.innerHTML = 'Position could not be found';
+    });
 }
